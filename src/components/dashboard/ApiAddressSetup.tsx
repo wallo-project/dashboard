1ac236bd1bf95f8b6c84403247a6c2069dc38e3b
benjaminpmd <dev.benjaminpaumard@gmail.com>
2023-03-09 10:38:52 +0100

    add documentation to first component

--- src/components/dashboard/ApiAddressSetup.tsx
+++ src/components/dashboard/ApiAddressSetup.tsx
@@ -3,19 +3,26 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { apiActions } from "../../store/apiSlice";
 
+/**
+ * Component that returns the interface to setup the API address.
+ * 
+ * @returns a JSX.Element
+ */
 export default function ApiAddressSetup() {
+  // use dispatch from the redux store
   const dispatch = useDispatch();
 
-
+  //  tailwind classes
   const labelClasses: string = "block m-2";
-  const inputClasses: string =
-    "block object-center mx-auto p-2 m-2 w-44 bg-gray-300 dark:bg-gray-600 rounded-xl";
+  const inputClasses: string = "block object-center mx-auto p-2 m-2 w-44 bg-gray-300 dark:bg-gray-600 rounded-xl";
 
+  // use state to dynamically update values
   const [ipValue, setIpValue] = useState("localhost");
   const [portValue, setPortValue] = useState("8080");
   const [message, setMessage] = useState("");
   const [error, setErrorStatus] = useState(false);
 
+  // create handlers for input changes
   const handleIpChange = (event: any) => {
     setIpValue(event.target.value);
   };
@@ -24,30 +31,37 @@ export default function ApiAddressSetup() {
     setPortValue(event.target.value);
   };
 
+  // handle api config (called when the user submit the informations)
   const handleApiConfig = (e: any) => {
+    // prevent reload
     e.preventDefault()
+    // disable errors
     setErrorStatus(false);
     setMessage("Loading...");
+    // try to call the API
     try {
       axios
         .get(`http://${ipValue}:${portValue}/healthcheck`)
         .then((response: any) => {
-          console.log(response)
           if (response.status === 200) {
+            // if the response is OK, dispatch the address of the API
             dispatch(
               apiActions.setApiAddress({
                 ipAddress: ipValue,
                 port: portValue,
               })
             );
+            // dispatch the connection as established
             dispatch(apiActions.setConnectionStatus(true));
           }
         })
         .catch((e) => {
+            // catch error on API response
             setErrorStatus(true);
-          setMessage("Error: Could not establish connection with the API");
+            setMessage("Error: Could not establish connection with the API");
         });
     } catch (e) {
+        // catch error on API call
         setErrorStatus(true);
       setMessage("Error: Could not establish connection with the API");
     }
